edc3707565f8f42eebbb4fcfa11de8d2d4e42635
Teofil <theoburdach@gmail.com>
2017-07-18 16:35:19 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/a/theoshop/data/ItemContract.java
@@ -0,0 +1,41 @@
+package com.example.a.theoshop.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by a on 18-Jul-17.
+ */
+
+public final class ItemContract {
+    public ItemContract() {
+    }
+    //Content provider. package name for the app
+    public static final String CONTENT_AUTHORITY = "com.example.a.theoshop.items";
+    //Base for all URI which app will use to contact the content provider
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    //possible path base content URI for possible URI's
+    public static final String PATH_ITEMS = "items";
+    //Inner class to define constant values in items database
+    public static final class ItemEntry implements BaseColumns{
+        //content URI to access items data in the table
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_ITEMS);
+        //MIME typr of the content uri for list of items
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE +
+                "/" + CONTENT_AUTHORITY + "/" + PATH_ITEMS;
+        //MIME type for single pet
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+                + "/" + CONTENT_AUTHORITY + "/" + PATH_ITEMS;
+        public static final String TABLE_NAME = "items";
+        public final static String _ID= BaseColumns._ID;
+        public final static String COLUMN_ITEM_NAME= "name";
+        public final static String COLUMN_ITEM_PRICE= "price";
+        public final static String COLUMN_ITEM_QUANTITY= "quantity";
+        public final static String COLUMN_ITEM_IMAGE= "image";
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/a/theoshop/data/ItemDbHelper.java
@@ -0,0 +1,42 @@
+package com.example.a.theoshop.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.a.theoshop.data.ItemContract.ItemEntry;
+
+/**
+ * Created by a on 18-Jul-17.
+ */
+
+public class ItemDbHelper extends SQLiteOpenHelper {
+    public static final String LOG_TAG = ItemDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "inventory.db";
+    private static int DATABASE_VERSION = 1;
+    public ItemDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String TYPE_TEXT = " TEXT";
+        final String TYPE_FLOAT = " REAL";
+        final String TYPE_INTEGER = " INTEGER";
+        final String TYPE_COMMA = ", ";
+        String CREATE_ITEMS_TABLE_SQL = "CREATE TABLE " +
+                ItemEntry.TABLE_NAME +
+                " (" +
+                ItemEntry._ID + TYPE_INTEGER + "PRIMARY KEY AUTOINCREMENT"+ TYPE_COMMA +
+                ItemEntry.COLUMN_ITEM_NAME + TYPE_TEXT+"NOT NULL"+ TYPE_COMMA+
+                ItemEntry.COLUMN_ITEM_PRICE + TYPE_FLOAT +"NOT NULL"+ TYPE_COMMA +
+                ItemEntry.COLUMN_ITEM_QUANTITY + TYPE_INTEGER + "NOT NULL"+ TYPE_COMMA +
+                ItemEntry.COLUMN_ITEM_IMAGE + TYPE_TEXT + "NOT NULL" + ");";
+        db.execSQL(CREATE_ITEMS_TABLE_SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
